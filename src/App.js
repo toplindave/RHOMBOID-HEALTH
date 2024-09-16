9e49bb76b5f28bbfe432ccf87d18cdc469aea22d
toplindave <adewaledamilare200@gmail.com>
2024-09-16 20:38:43 +0100

    now

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import "./App.css";
 // import Body from "./component/body/Body";
-// import Nav from "./component/nav/Nav";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Sign from "./component/sign-in/Sign-in";
-// import Register from "./pages/register/Register";
-// import Free from "./pages/free/Free";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sign from "./component/sign-in/Sign-in";
+import Register from "./pages/register/Register";
+import Free from "./pages/free/Free";
 import LandingPage from "./pages/landing/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
-      {/* <Free /> */}
-      {/* <BrowserRouter> */}
-      {/* <Nav /> */}
-      {/* <Routes> */}
-      {/* <Body /> */}
-      {/* <Route path="/" element={<LandingPage />} /> */}
-      {/* <Route path="/account/sign-in" element={<Sign />} /> */}
-      {/* <Route path="/account/register" element={<Register />} /> */}
-      {/* <Route path="/free" element={<Free />} /> */}
-      {/* </Routes> */}
-      {/* </BrowserRouter> */}
+      <BrowserRouter>
+        <Routes>
+          {/* <Body /> */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/account/sign-in" element={<Sign />} />
+          <Route path="/account/register" element={<Register />} />
+          <Route path="/free" element={<Free />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -6,31 +6,33 @@ import RegisterAddress from "./RegisterAddress";
 import VerifyNumber from "./VerifyNumber";
 import VerifyCode from "./VerifyCode";
 import Welcome from "./Welcome";
+import Nav from "../../component/nav/Nav";
 
 function Register() {
-	const [formStep, setFormStep] = useState(0);
-	const nextStep = (e) => {
-		e.preventDefault();
-		setFormStep((cur) => cur + 1);
-	};
-	const prevStep = (e) => {
-		e.preventDefault();
-		setFormStep((cur) => cur - 1);
-	};
-	return (
-		<>
-			{formStep === 0 && <RegisterEmail nextStep={nextStep} />}
-			{formStep === 1 && (
-				<RegisterName nextStep={nextStep} prevStep={prevStep} />
-			)}
-			{formStep === 2 && (
-				<RegisterAddress nextStep={nextStep} prevStep={prevStep} />
-			)}
-			{formStep === 3 && <VerifyNumber nextStep={nextStep} />}
-			{formStep === 4 && <VerifyCode nextStep={nextStep} />}
-			{formStep === 5 && <Welcome />}
-		</>
-	);
+  const [formStep, setFormStep] = useState(0);
+  const nextStep = (e) => {
+    e.preventDefault();
+    setFormStep((cur) => cur + 1);
+  };
+  const prevStep = (e) => {
+    e.preventDefault();
+    setFormStep((cur) => cur - 1);
+  };
+  return (
+    <>
+      <Nav />
+      {formStep === 0 && <RegisterEmail nextStep={nextStep} />}
+      {formStep === 1 && (
+        <RegisterName nextStep={nextStep} prevStep={prevStep} />
+      )}
+      {formStep === 2 && (
+        <RegisterAddress nextStep={nextStep} prevStep={prevStep} />
+      )}
+      {formStep === 3 && <VerifyNumber nextStep={nextStep} />}
+      {formStep === 4 && <VerifyCode nextStep={nextStep} />}
+      {formStep === 5 && <Welcome />}
+    </>
+  );
 }
 
 export default Register;
